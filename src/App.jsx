fca3260957e90c0f03cb61ff053407278a9b83b3
Adis9999 <takabaevadis2009@gmail.com>
2025-01-20 17:03:14 +0600

    save

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
     {
       title: "Мужские Кроссовки Puma RS-X3",
       image:
-      "https://colehaan.ae/cdn/shop/files/BaseTransform1_5fc0ba6b-dc61-48c5-be0b-bdcfb3b977fd_1200x1200.jpg?v=1717503802",
+        "https://colehaan.ae/cdn/shop/files/BaseTransform1_5fc0ba6b-dc61-48c5-be0b-bdcfb3b977fd_1200x1200.jpg?v=1717503802",
       price: "10 499 som",
     },
     {
@@ -32,23 +32,27 @@ const App = () => {
       image:
         "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_1000,h_1000/global/389383/06/sv01/fnd/THA/fmt/png/Bari-Casual-Canvas-Sneakers",
       price: "11 999 som",
-    },{
+    },
+    {
       title: "Женские Кроссовки Reebok Classic Leather",
       image:
         "https://img.freepik.com/free-photo/one-white-sneaker-shoe-isolated-white_93675-133980.jpg",
       price: "8 499 som",
-    },{
+    },
+    {
       title: "Женские Кроссовки Reebok Classic Leather",
       image:
         "https://img.freepik.com/premium-photo/sneakers-isolated-white-background_236836-23394.jpg",
       price: "8 499 som",
-    },{
+    },
+    {
       title: "Женские Кроссовки Reebok Classic Leather",
       image:
         "https://img.freepik.com/free-photo/one-white-sneaker-shoe-isolated-white_93675-134695.jpg",
       price: "8 499 som",
     },
   ];
+
   return (
     <div className="cards">
       {products.map((item) => (
